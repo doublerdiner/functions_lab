dde9723c866cc3e24f5a6e417b06d51fccbb9b64
Chris Barclay <chris.barclay1@gmail.com>
2023-01-25 15:27:19 +0000

    Completed divide

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -10,4 +10,7 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     return num1 * num2
 
-    
+def divide(num1, num2):
+    return int(num1/num2)
+#could also use // to just return the nearest whole number and ignore any remainder.
+
